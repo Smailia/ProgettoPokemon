3056d1651057b3510abc798729ec102996ecb95d
Smailia <smailia.omar@iisgalvanimi.edu.it>
2025-05-06 20:10:51 +0000

    3

--- assetloader.js
+++ assetloader.js
@@ -12,6 +12,40 @@ function loadAssets() {
         "mushroom-mon": { x: 32, y: 49, width: 32, height: 32 },
         "mini-mons": { x: 0, y: 0, width: 128, height: 16, sliceX: 8, sliceY: 1, 
             anims: { "spider": 1, "centipede": 2, "grass": 3}
-        }
+        },
     })
+loadSprite("battle-background", "./assets/battleBackground.png");
+loadSpriteAtlas("./assets/tiles.png", {
+    tile: {
+    x: 0,
+    y: 0,
+    width: 128,
+    height: 128,
+    sliceX: 8,
+    sliceY: 8,
+    anims: {
+        "bigtree-pt1": 1,
+        "bigtree-pt2": 2,
+        "bigtree-pt3": 9,
+        "bigtree-pt4": 10,
+        "grass-m": 14,
+        "grass-tl": 17,
+        "grass-tm": 18,
+        "grass-tr": 19,
+        "grass-l": 25,
+        "grass-r": 27,
+        "grass-bl": 33,
+        "grass-mb": 34,
+        "grass-br": 35,
+        "tree-t": 4,
+        "tree-b": 12,
+        "grass-water": 20,
+        "sand-1": 6,
+        "ground-l": 41,
+        "ground-m": 42,
+        "ground-r": 43,
+        "rock-water": 60,
+    },
+  },
+ });
 }
